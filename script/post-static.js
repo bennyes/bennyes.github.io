637e5ddc70fb8a89b0215bbe46e537e0de7cef79
bennyes <ben_3223@yahoo.com>
2021-11-06 23:10:11 +0700

    Update post-static.js

--- script/post-static.js
+++ script/post-static.js
@@ -27,7 +27,7 @@ var ol=document.createElement('ol');dft.appendChild(ol);var parts=document.getEl
                 var lia=document.createElement('a');
                 lia.href=`#${jump.id}`;
                 var th=h2[i].innerHTML.split('. ')
-                var ch=new RegExp("[0-9]+");
+                var ch=new RegExp("/^[0-9]+\.");
                 lia.innerHTML=ch.test(th[0])?lia.innerHTML=th[1]:lia.innerHTML=th[0],h2[i].innerHTML=`${ut}. ${h2[i].innerHTML}`;
                 li.appendChild(lia);
                 var parts2=parts[h].getElementsByClassName('parts');
