d8c7f9b0a5ee1a9f2d5ffab7c5c37ed7e4798bb3
bennyes <ben_3223@yahoo.com>
2021-11-27 15:57:35 +0700

    Update latblprint.js

--- script/latblprint.js
+++ script/latblprint.js
@@ -3,7 +3,7 @@ function liGen(cdw,i,ul){
     for(var j=0;j<cdw.length;j++){
         var li=document.createElement('li');
         ul.appendChild(li);
-        var ais=cdw.replace(/\s+/g, '-').toLowerCase();
+        var ais=cdw[j].replace(/\s+/g, '-').toLowerCase();
         var a=document.createElement('a');
         a.href=`/${ais}`;
         a.innerHTML=cdw[j].charAt(0).toUpperCase() + cdw[j].slice(1);;
