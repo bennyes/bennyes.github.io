c73b3872158276acb8393ea705567d58f2bfd9e3
bennyes <89200748+bennyes@users.noreply.github.com>
2024-01-05 10:53:48 +0700

    Delete biaya directory

--- biaya/getterbiaya.js
+++
@@ -1,1 +0,0 @@
-function gettoldata(f,r,g,m,k){const tol=getjsonfile(`https://bennyes.github.io/biaya/tol/${f}.json`);let type=tol[r]["type"];let t=(type==="tutup")?tol[r][m][k][g]:tol[r][g];return t;}
--- biaya/tol/getter.js
+++
@@ -1,6 +0,0 @@
-function gettoldata(f,r,g,m,k){
-    const tol=getjsonfile(`https://bennyes.github.io/biaya/tol/${f}.json`);
-    let type=tol[r]["type"];
-    let tarif=(type==="closed")?tol[r][m][k][g]:tol[r][g];
-    return tarif;
-}
