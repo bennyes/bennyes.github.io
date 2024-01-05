587075f398b580778aa571100651445c57642d09
bennyes <89200748+bennyes@users.noreply.github.com>
2024-01-05 11:10:57 +0700

    Create main.js

---
+++ script/main.js
@@ -0,0 +1,14 @@
+function cssloader(csslink,rel='stylesheet',crossorigin=false){
+  let css=document.createElement('link');
+  css.rel=rel;
+  css.href=csslink;
+  if(crossorigin){
+    css.crossOrigin='crossorigin';
+  }
+  document.head.appendChild(css);
+}
+function load_head(pagetype='home'){
+  cssloader('https://fonts.googleapis.com','preconnect');
+  cssloader('https://fonts.gstatic.com','preconnect');
+  cssloader('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;600;700&family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+}
