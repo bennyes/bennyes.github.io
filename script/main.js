e26cd0bb9fc8b24416b51bc27d5474ccae481256
bennyes <89200748+bennyes@users.noreply.github.com>
2024-01-05 11:35:36 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -35,5 +35,6 @@ function load_head(pagetype='home'){
   cssloader('https://fonts.googleapis.com','preconnect');
   cssloader('https://fonts.gstatic.com','preconnect','crossorigin');
   cssloader('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;600;700&family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+  cssloader(`/styles/main.css`);
   cssloader(`/styles/${pagetype}.css`);
 }
